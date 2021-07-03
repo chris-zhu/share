5f3c06e3c075f9bfee47650efbd3a68b9ac0e558
chris <1633711653@qq.com>
2021-07-03 14:11:12 +0800

    update

--- components/vite.vue
+++ components/vite.vue
@@ -2,9 +2,9 @@
   <div class="body">
     <div class="left">
       <div class="vite">
-        <img @click="num++" src="/vite.svg" alt="vite"/>
+        <img @click="num++" src="/vite.svg" alt="vite" />
         <p>
-          <span @click="num--">learn more at </span> 
+          <span @click="num--">learn more at</span>
           <n-a href="https://cn.vitejs.dev/" target="_blank">https://cn.vitejs.dev</n-a>
         </p>
       </div>
@@ -13,20 +13,20 @@
       <div v-if="showPics">
         <n-image-group>
           <n-space>
-            <n-image
-              width="500"
-              src="/viteWebpack001.png"
-            />
-            <n-image
-              width="500"
-              src="/viteWebpack002.png"
-            />
+            <n-image width="500" src="/viteWebpack001.png" />
+            <n-image width="500" src="/viteWebpack002.png" />
           </n-space>
         </n-image-group>
       </div>
+
       <n-ul align-text>
         <n-li v-if="num >= 1">类似vue-cli的熟悉体验</n-li>
-        <n-li v-if="num >= 2">基于原生ESM的极速热更新</n-li>
+        <n-li v-if="num >= 2">
+          基于原生ESM的极速热更新
+          <div v-if="num == 2">
+            <n-image src="http://chat.chrisying.cn/1625292332313-3947.jfif" />
+          </div>
+        </n-li>
         <n-li v-if="num >= 3">基于esbuild的依赖预打包</n-li>
         <n-li v-if="num >= 4">兼容rollup的插件机制</n-li>
         <n-li v-if="num >= 5">内置SSR支持</n-li>
@@ -37,8 +37,8 @@
 </template>
 
 <script lang='ts' setup>
-import { ref,computed } from 'vue'
-import {NUl, NLi, NA, NImage, NImageGroup, NSpace} from 'naive-ui'
+import { ref, computed } from 'vue'
+import { NUl, NLi, NA, NImage, NImageGroup, NSpace } from 'naive-ui'
 const num = ref(0)
 const showPics = computed(() => {
   return num.value <= 0
@@ -59,8 +59,8 @@ const showPics = computed(() => {
 img {
   width: 150px;
 }
-.right{
-  margin-left:100px;
-  width:400px;
+.right {
+  margin-left: 100px;
+  width: 400px;
 }
 </style>
